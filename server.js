3b956221435a8e6c9df539da0e07de550d676eb3
Leland Hayes <lelandkhayes13@gmail.com>
2021-08-25 17:38:37 -0400

    pushing all initial files

---
+++ server.js
@@ -0,0 +1,6 @@
+// Imports inquirer package
+const inquirer = require('inquirer');
+// Imports mysql package
+const mysql = require('mysql2');
+// Imports console table package
+const cTable = require('console.table');
