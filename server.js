ba6e5f795d3e43c9b1787ab3325c968497d7b721
Leland Hayes <lelandkhayes13@gmail.com>
2021-08-27 18:33:35 -0400

    updated questions, also added inquirer function to prompt questions

--- server.js
+++ server.js
@@ -4,3 +4,56 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 // Imports console table package
 const cTable = require('console.table');
+// Importing questions
+const questions = require('./src/questions');
+
+const database = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: "company_db",
+  });
+
+database.connect(function(err) {
+    if (err) {
+        console.log('Something went wrong..');
+        start();
+        return;
+    }
+    console.log(res)
+});
+
+// start function to prompt questions
+const start = () => {
+    return inquirer.prompt(questions);
+    try {
+        switch (val.action) {
+            case "View all departments?":
+                console.log(val.action);
+                viewAllEmployees();
+                break;
+            
+            case "View all roles?":
+                break;
+            
+            case "View all employees?":
+                break;
+
+            case "Add a department?":
+                break;
+
+            case "Add a role?":
+                break;
+
+            case "Add a employee?":
+                break;
+
+            case "Update an employee role?":
+                break;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
--- src/questions.js
+++ src/questions.js
@@ -2,17 +2,17 @@ const questions = [
     {
         name: "choice",
         type: "list",
-        message: "What do you want to do?",
+        message: "What would you like to do?",
         choices: [
-            "View all departments", 
-            "View all roles", 
-            "View all employees",
-            "Add a department",
-            "Add a role",
-            "Add a employee",
-            "Update an employee role",
+            "View all departments?", 
+            "View all roles?", 
+            "View all employees?",
+            "Add a department?",
+            "Add a role?",
+            "Add a employee?",
+            "Update an employee role?",
         ]
     }
-]
+];
 
 module.exports = questions;
